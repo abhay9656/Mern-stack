8655578ce70364535aabda95dd921eabbffcc510
abhay9656 <abhay9656@gmail.com>
2024-03-29 15:08:05 +0530

    defining scope

---
+++ Section 1/keyword.js
@@ -0,0 +1,13 @@
+ n=248
+
+ if(n%2===0)
+ {
+    console.log('even');
+    num="this should be global"
+    let nu="good"
+    console.log(nu);
+ }
+ else{
+    console.log("false");
+ }
+ console.log(num);
