14fff251528f370a9670d7bfe269f384fc22f45e
abhay9656 <abhay9656@gmail.com>
2024-03-29 15:11:40 +0530

    changes the value

--- Section 1/keyword.js
+++ Section 1/keyword.js
@@ -5,7 +5,10 @@
     console.log('even');
     num="this should be global"
     let nu="good"
+    nu="nice"
     console.log(nu);
+    const pi=3.14
+    console.log(pi);
  }
  else{
     console.log("false");
