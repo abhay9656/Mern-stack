3506eb1150befffed430acc6939379b1f26e7cee
abhay9656 <abhay9656@gmail.com>
2024-03-31 13:44:51 +0530

    added shift

--- Section 1/array.js
+++ Section 1/array.js
@@ -3,9 +3,18 @@ console.log(my_array);
 console.log(typeof my_array);
 console.log(Array.isArray(my_array));
 
-const movie=['godzilla','deedpool','Dark knight','spiderman']
+const movie=['godzilla','deedpool','Dark knight','spiderman','animal']
 console.log(movie.length);
 console.log(movie.at(-2));
 console.log(movie.indexOf('spiderman'));
+console.log(movie.slice(0));
+console.log(movie.slice(-4,-2));
 
-console.log(movie.slice(1));
+movie.push('joker')
+console.log(movie);
+
+movie.unshift('avenger')
+console.log(movie);
+
+movie.shift()
+console.log(movie)
