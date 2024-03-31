4133f9467fb3a93682759e71b62410a7dfb5b1c8
abhay9656 <abhay9656@gmail.com>
2024-03-31 13:21:06 +0530

    created new file

---
+++ Section 1/array.js
@@ -0,0 +1,7 @@
+const my_array=['xt',233,true,undefined];
+console.log(my_array);
+console.log(typeof my_array);
+console.log(Array.isArray(my_array));
+
+const movie=['godzilla','deedpool','Dark knight','spiderman']
+console.log(movie.length);
