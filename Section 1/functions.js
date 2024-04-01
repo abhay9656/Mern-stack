9539b3565e8a62a796c108225bf625a4894ff1c0
abhay9656 <abhay9656@gmail.com>
2024-04-01 15:23:14 +0530

    perfect using function

--- Section 1/functions.js
+++ Section 1/functions.js
@@ -12,15 +12,34 @@
 // console.log(result);
 // console.log(fun(20,30,60));
 
-const factorial =(n)=>{
-    let i;
-let fact=1
-for(i=1;i<=n;i++)
-{
-    fact=fact*i
-}
-return fact
-}
+// const factorial =(n)=>{
+//     let i;
+// let fact=1
+// for(i=1;i<=n;i++)
+// {
+//     fact=fact*i
+// }
+// return fact
+// }
 
-const fact= factorial(7)
-console.log(fact);
+// const fact= factorial(7)
+// console.log(fact);
+
+// perfect square
+
+const perfect=(n)=>{
+    let sq=n**0.5
+    console.log(sq);
+    // console.log(Number.isInteger(sq));
+    return Number.isInteger(sq)
+}
+const pf=perfect(24)
+console.log(pf);
+if(pf)
+{
+    console.log("perfect ");
+}
+else
+{
+    console.log("not");
+}
