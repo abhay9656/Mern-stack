df8c9a4ba842dc4af5f3447b29ac544a77918bc6
abhay9656 <abhay9656@gmail.com>
2024-04-01 15:13:06 +0530

    arrow function

--- Section 1/functions.js
+++ Section 1/functions.js
@@ -1,13 +1,26 @@
-function abc(a,b){
-    c=a+b;
-    console.log(c);
-}
-abc(10,34)
+// function abc(a,b){
+//     c=a+b;
+//     console.log(c);
+// }
+// abc(10,34)
 
 
-const fun = function avg(m1,m2,m3){
-     return (m1+m2+m3)/3;
+// const fun = function avg(m1,m2,m3){
+//      return (m1+m2+m3)/3;
+// }
+// const result=fun(20,30,40)
+// console.log(result);
+// console.log(fun(20,30,60));
+
+const factorial =(n)=>{
+    let i;
+let fact=1
+for(i=1;i<=n;i++)
+{
+    fact=fact*i
 }
-const result=fun(20,30,40)
-console.log(result);
-console.log(fun(20,30,60));
+return fact
+}
+
+const fact= factorial(7)
+console.log(fact);
