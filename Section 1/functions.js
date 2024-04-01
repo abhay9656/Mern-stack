5a1f3a362c8248282bdc2d637a942b5332bc503a
abhay9656 <abhay9656@gmail.com>
2024-04-01 15:09:59 +0530

    change the code

--- Section 1/functions.js
+++ Section 1/functions.js
@@ -3,3 +3,11 @@ function abc(a,b){
     console.log(c);
 }
 abc(10,34)
+
+
+const fun = function avg(m1,m2,m3){
+     return (m1+m2+m3)/3;
+}
+const result=fun(20,30,40)
+console.log(result);
+console.log(fun(20,30,60));
