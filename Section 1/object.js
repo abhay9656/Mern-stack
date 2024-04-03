78a67d43c8b0ce720bcb4a348c3e9484baf9eea1
abhay9656 <abhay9656@gmail.com>
2024-04-03 14:44:12 +0530

    make changes in code

--- Section 1/object.js
+++ Section 1/object.js
@@ -1,22 +1,31 @@
 
 
-const obj={
-    name:'abc',
-    email:"abc@gmail.com",
-    password:"12345"
+// const obj={
+//     name:'abc',
+//     email:"abc@gmail.com",
+//     password:"12345"
+// }
+
+// console.log(obj);
+// console.log(obj.email);
+// console.log(obj['password']);
+
+// let key="name"
+// console.log(obj[key]);
+
+// obj.password="hello"
+// console.log(obj);
+// obj.address="lucnow"
+// console.log(obj);
+
+// console.log( Object.keys(obj) );
+// console.log( Object.values(obj));
+
+
+const smartphone={
+    brand:"Samsung",
+    model:'S24',
+    price:120000,
+    color:['white','black','blue']
 }
-
-console.log(obj);
-console.log(obj.email);
-console.log(obj['password']);
-
-let key="name"
-console.log(obj[key]);
-
-obj.password="hello"
-console.log(obj);
-obj.address="lucnow"
-console.log(obj);
-
-console.log( Object.keys(obj) );
-console.log( Object.values(obj));
+console.log(smartphone);
