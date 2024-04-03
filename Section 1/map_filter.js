3b1d2f9fee86cfda7e974ead055051718c5de979
abhay9656 <abhay9656@gmail.com>
2024-04-03 15:25:19 +0530

    converted into upper case

--- Section 1/map_filter.js
+++ Section 1/map_filter.js
@@ -1,9 +1,22 @@
 
 const num=[4,5,7,8,9,12]
 
-const newNums=[];
-for(let n of num)
-{
-    newNums.push(n**3)
-}
-console.log(newNums);
+// const newNums=[];
+// for(let n of num)
+// {
+//     newNums.push(n**3)
+// }
+// console.log(newNums);
+
+// const newnum = num.map( (a)=>{
+//     return a**3
+// })
+
+
+const name=['adarsh','shaurya','avijeet','yash','atul','arpit']
+ const upper=name.map((b)=>{
+    return b.toUpperCase()
+ })
+
+ console.log(upper);
+// console.log(newnum);
