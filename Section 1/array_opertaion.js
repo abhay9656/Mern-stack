29b6e2da015542faa3b816e81dc21f7d57517788
abhay9656 <abhay9656@gmail.com>
2024-04-04 15:04:58 +0530

    Example

---
+++ Section 1/array_opertaion.js
@@ -0,0 +1,36 @@
+const phonearray=[
+    {
+        brand:"Samsung",
+        model:'S24',
+        price:120000,
+        color:['white','black','graphite']   
+    },
+    {
+        brand:"Apple",
+        model:'iphone 15',
+        price:12000,
+        color:['black','blue']   
+    },
+    {
+        brand:"Realme",
+        model:'narzo',
+        price:1200,
+        color:['green','black','blue']   
+    },
+    {
+        brand:"Samsung",
+        model:'Z-fold',
+        price:164000,
+        color:['gray','black','green']   
+    }
+]
+
+// const fil=phonearray.filter((pri)=>{
+//           return pri.brand==="Samsung"
+// })
+// console.log(fil);
+
+const col=phonearray.filter((cl)=>{
+    return cl.color.includes("blue")
+})
+console.log(col);
