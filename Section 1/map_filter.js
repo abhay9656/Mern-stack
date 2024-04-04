6052fffe0ccd0785ae278d5fce3110b4b9813246
abhay9656 <abhay9656@gmail.com>
2024-04-04 14:26:00 +0530

    filter

--- Section 1/map_filter.js
+++ Section 1/map_filter.js
@@ -13,10 +13,16 @@ const num=[4,5,7,8,9,12]
 // })
 
 
-const name=['adarsh','shaurya','avijeet','yash','atul','arpit']
- const upper=name.map((b)=>{
-    return b.toUpperCase()
- })
+// const name=['adarsh','shaurya','avijeet','yash','atul','arpit']
+//  const upper=name.map((b)=>{
+//     return b.toUpperCase()
+//  })
 
- console.log(upper);
-// console.log(newnum);
+//  console.log(upper);
+// // console.log(newnum);
+
+ //filter
+const even= num.filter((n)=>{
+   return n%2===0
+})
+console.log(even);
