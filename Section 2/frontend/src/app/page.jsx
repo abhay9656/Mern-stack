2d00025c8796ec90de761469032414a86f69fc24
abhay9656 <abhay9656@gmail.com>
2024-04-10 15:00:24 +0530

    added   image

--- Section 2/frontend/src/app/page.jsx
+++ Section 2/frontend/src/app/page.jsx
@@ -4,7 +4,8 @@ import classes from './page.module.css'
 const Home = () => {
   return (
     <div className={classes.bg}>
-        <h1 className={classes.link} style={{ textAlign:'center',fontSize:60 }}>Welcome page</h1>
+        <h1 className={classes.link} style={{ textAlign:'center',fontSize:60 }}>Welcome page
+        </h1>
     </div>
   );
 }
