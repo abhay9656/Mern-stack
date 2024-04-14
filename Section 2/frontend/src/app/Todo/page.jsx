63564d8959276bd5203c2e7355be31b42f92d097
abhay9656 <abhay9656@gmail.com>
2024-04-14 13:57:22 +0530

    working with usestate

--- Section 2/frontend/src/app/Todo/page.jsx
+++ Section 2/frontend/src/app/Todo/page.jsx
@@ -1,9 +1,33 @@
-import React from 'react';
+'use client'
+import React, { useState } from 'react';
 
 const Todo = () => {
+
+     const [todolist, setTodolist] = useState([
+        { task:'Do homeWork',completed:false },
+        { task:'Eat',completed:false },
+        { task:'watch movies',completed:false }
+     ])     
+    const addtask=(e)=>{
+        if(e.code ==='Enter'){
+            console.log(e.target.value);
+        }
+       
+    }
+
+
   return (
-    <div>
-      Todo
+    <div className='container py-5'>
+     <h2 className='text-center'>Todo App</h2>
+     <hr />
+        <div className="card">
+            <div className="card-header">
+                <input onKeyDown={addtask} type="text" className='form-control border-primary border-2'/>
+            </div>
+            <div className="card-body">
+                
+            </div>
+        </div>
     </div>
   );
 }
