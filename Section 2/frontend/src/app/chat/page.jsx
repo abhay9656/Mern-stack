177ff1b521dc59a0e095557e1f91564aac8173c7
abhay9656 <abhay9656@gmail.com>
2024-04-16 10:04:03 +0530

    added useState

--- Section 2/frontend/src/app/chat/page.jsx
+++ Section 2/frontend/src/app/chat/page.jsx
@@ -1,18 +1,32 @@
+'use client'
 import React from 'react';
 import styles from './chat.module.css'
+import  { useState } from 'react';
 const Chat = () => {
+  
+    const [chat, setchat] = useState([{
+        chat:"hello! i  m here"
+    }])
+
+    const msg=()=>{
+        
+    }
+
   return (
     <div>
       <div className={styles.chatcontainer}>
         <div className={`${styles.chatheader}`}>Chat Page</div>
-        <div className="chat-messages">
-          <div className="message sender">Hello!</div>
-          <div className="message receiver">Hi there!</div>
-          <div className="message sender">How are you?</div>
-          <div className="message receiver">I'm good, thank you!</div>
+        <div className='p-5'>
+           
+           {
+            chat.map((item)=>{
+                return <p>{item.chat}</p>
+            })
+           }
+         
         </div>
         <div className={styles.chatinput}>
-          <input className={styles.input} type="text" placeholder="Type your message..." />
+          <input className={styles.input} onKeyDown={msg} type="text" placeholder="Type your message..." />
         </div>
       </div>
     </div>
