f858423944467496554afd575c28072d95287148
abhay9656 <abhay9656@gmail.com>
2024-04-16 09:46:11 +0530

    added css

--- Section 2/frontend/src/app/chat/page.jsx
+++ Section 2/frontend/src/app/chat/page.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-
+import styles from './chat.module.css'
 const Chat = () => {
   return (
     <div>
-      <div className="chat-container">
-        <div className="chat-header">Chat Page</div>
+      <div className={styles.chatcontainer}>
+        <div className={`${styles.chatheader}`}>Chat Page</div>
         <div className="chat-messages">
           <div className="message sender">Hello!</div>
           <div className="message receiver">Hi there!</div>
           <div className="message sender">How are you?</div>
           <div className="message receiver">I'm good, thank you!</div>
         </div>
-        <div className="chat-input">
+        <div className={styles.chatinput}>
           <input type="text" placeholder="Type your message..." />
           <button>Send</button>
         </div>
