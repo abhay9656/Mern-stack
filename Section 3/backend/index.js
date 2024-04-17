56d3dfb7c9edfce17e658109b3de011c620a52fb
abhay9656 <abhay9656@gmail.com>
2024-04-17 15:29:05 +0530

    make route

--- Section 3/backend/index.js
+++ Section 3/backend/index.js
@@ -4,5 +4,21 @@ const express = require('express')
 // initialize express
 const app =express();
 const port =5000;
+ 
+//endpoint route
+app.get('/',(req,res)=>{
+    res.send("Response form express")
+})
 
+app.get('/add',(req,res)=>{
+    res.send("Response from add")
+})
+
+app.get('/delete',(req,res)=>{
+    res.send("response form delete")
+})
+
+app.get('/update',(req,res)=>{
+    res.send("response form update")
+})
 app.listen(port, ()=>{console.log('server started');});
