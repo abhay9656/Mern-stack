ab5b360c5974846dcfebd0d3c36e093c99fcd942
abhay9656 <abhay9656@gmail.com>
2024-04-24 15:12:26 +0530

    created a model for user

--- Section 3/backend/models/userModels.js
+++ Section 3/backend/models/userModels.js
@@ -1,1 +1,8 @@
-const {}=require('../connection');
+const { model,Schema }=require('../connection');
+const mychema=new Schema({
+    name:{type:String,require:true},
+    email:{type:String,unique:true},
+    password:String,
+    createdAt:{type:Date,default:Date.now}
+})
+module.exports=model('user',mychema)
