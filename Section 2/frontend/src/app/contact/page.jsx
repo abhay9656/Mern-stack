7776e1a6ec065f3ad96743248ecd9924b7dbcd18
abhay9656 <abhay9656gupta@gmail.com>
2024-06-05 12:23:24 +0530

    Refactor landing page HTML and contact page JSX

--- Section 2/frontend/src/app/contact/page.jsx
+++ Section 2/frontend/src/app/contact/page.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
-import styles from './contact.module.css'
-const Contact = () => {
+import React from 'react';
+
+
+const App = () => {
   return (
-    <div>
-    contact
+    <div className="flex flex-wrap">
+      <div className="w-full sm:w-1/2 p-4">
+        <img className="mx-auto w-1/2 sm:w-full" src="https://via.placeholder.com/150" alt="Placeholder" />
+      </div>
+      <div className="w-full sm:w-1/2 p-4">
+        <h2 className="text-xl font-bold mb-2">Feedback</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet lacus enim.</p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Contact
+export default App;
