40d8345ad6d6a66c4b39a177006d97418fc37f61
abhay9656 <abhay9656gupta@gmail.com>
2024-06-21 15:27:36 +0530

    Refactor user model to include name, email, password, and createdAt fields

--- Section 3/backend_revision/models/userModel.js
+++ Section 3/backend_revision/models/userModel.js
@@ -1,1 +1,9 @@
 const {Schema ,model}=require('../connection')
+
+const mySchema=new Schema({
+    name:{type:String,required:true},
+    email:{type:String,required:true},
+    password:{type:String,required:true},
+    createdAt:{type:Date,default:Date.now}
+})
+module.exports=model('user',mySchema)
