4e898fd687514a73918ba67d49d0c26e46a6f8bb
abhay9656 <abhay9656gupta@gmail.com>
2024-06-21 15:12:39 +0530

    Add database connection code in connection.js

---
+++ Section 3/backend_revision/connection.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const url="mongodb+srv://abhayJS:abhayMern@cluster0.6kxwugb.mongodb.net/backend?retryWrites=true&w=majority&appName=Cluster0"
+mongoose.connect(url)
+.then((result) => {
+    console.log('connected to database');
+}).catch((err) => {
+    console.log(err);
+});
