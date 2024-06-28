af26f9cc18ed69eae88ccafbf4872b1a515475ba
abhay9656 <abhay9656gupta@gmail.com>
2024-06-28 15:52:35 +0530

    Refactor Navbar.jsx to update home link URL

--- Section 2/frontend/src/app/Navbar.jsx
+++ Section 2/frontend/src/app/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <Link className="nav-link "  href="#">
+          <Link className="nav-link "  href="/">
             Home
           </Link>
         </li>
