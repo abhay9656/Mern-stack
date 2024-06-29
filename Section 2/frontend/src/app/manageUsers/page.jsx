0c237416ebaf940c750efc3f5fa72857e06a732e
abhay9656 <abhay9656gupta@gmail.com>
2024-06-29 13:49:38 +0530

    Refactor manageUsers page component and add user list table in page.jsx

--- Section 2/frontend/src/app/manageUsers/page.jsx
+++ Section 2/frontend/src/app/manageUsers/page.jsx
@@ -1,20 +1,52 @@
-import React, { useEffect } from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
 
 const MangeUsers = () => {
 
+     const [userList,setUserList]=useState([])
 
     const fetchUser= async ()=>{
      const res= await fetch('http://localhost:5500/user/getall',{
         method:'GET'
       })
       console.log(res.status);
+      const data = await res.json()
+      console.table(data)
+      setUserList(data)
     }
 
-    useEffect
+    useEffect(()=>{
+       fetchUser();
+    },[])
 
   return (
     <div>
-      hello
+      <h3 className='mt-5 text-center fw-bold'>Manage User</h3>
+      <div className='conatainer'>
+          <table className='table table-dark'>
+            <thead>
+                <tr>
+                <th>Id</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Created At</th></tr>
+            </thead>
+            <tbody>
+                {
+                    userList.map((user)=>{
+                        return (
+                        <tr key={user._id}>
+                            <td>{user._id}</td>
+                            <td>{user.name}</td>
+                            <td>{user.email}</td>
+                            <td>{user.createdAt}</td>
+                        </tr>
+                        )
+                    })
+                }
+            </tbody>
+          </table>
+      </div>
     </div>
   )
 }
