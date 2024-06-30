168619cce0cb67de012e9c4c752ca29da4859df8
abhay9656 <abhay9656gupta@gmail.com>
2024-06-30 13:03:49 +0530

    Refactor product details form and update form fields in page.jsx

--- Section 2/frontend/src/app/product/page.jsx
+++ Section 2/frontend/src/app/product/page.jsx
@@ -6,88 +6,87 @@ import toast from 'react-hot-toast';
 const product = () => {
 
 
-    const productDetails =useFormik({
-        initialValues:{
-            productName:'',
-            productPrice:'',
-            productCategory:'',
-            productImage:''
+  const productDetails = useFormik({
+    initialValues: {
+      name: '',
+      price: '',
+      category: '',
+      image: ''
+    },
+    onSubmit: (values) => {
+      console.log(values);
+      fetch('http://localhost:5500/product/add', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
         },
-        onSubmit:(values)=>{
-            console.log(values);
-          fetch('http://localhost:5500/product/add',{
-              method:'POST',
-              headers:{
-                  'content-Type':'application/json'
-              },
-              body:JSON.stringify(values)
-          })
-          .then((response) => {
-            console.log(response.status);
-            if(response.status===200)
-            {
-              toast.success('Product added successfully')
-            }
-          }).catch((err) => {
-             console.log(err);
-              toast.error("Something went wrong")
-          });
-         
-        },
-    })
+        body: JSON.stringify(values)
+      })
+        .then((response) => {
+          console.log(response.status);
+          if (response.status === 200) {
+            toast.success('Product added successfully')
+          }
+        }).catch((err) => {
+          console.log(err);
+          toast.error("Something went wrong")
+        });
+
+    },
+  })
 
   return (
     <div>
       <div className="container mt-5">
-      <h2 className="text-center mb-4">Add Product</h2>
-      <form onSubmit={productDetails.handleSubmit}>
-        <div className="mb-3">
-          <label htmlFor="productName" className="form-label">Product Name</label>
-          <input
-            type="text"
-            className="form-control"
-            id="productName"
-            onChange={productDetails.handleChange}
-            value={productDetails.values.productName}
-            required
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="productPrice" className="form-label">Product Price</label>
-          <input
-            type="number"
-            className="form-control"
-            id="productPrice"
-            onChange={productDetails.handleChange}
-            value={productDetails.values.productPrice}
-            required
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="productCategory" className="form-label">Product Category</label>
-          <input
-            type="text"
-            className="form-control"
-            id="productCategory"
-           onChange={productDetails.handleChange}
-           value={productDetails.values.productCategory}
-            required
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="productImage" className="form-label">Product Image</label>
-          <input
-            type="text"
-            className="form-control"
-            id="productImage"
-            onChange={productDetails.handleChange}
-            value={productDetails.values.productImage}
-            required
-          />
-        </div>
-        <button type="submit" className="btn btn-primary w-100">Submit</button>
-      </form>
-    </div>
+        <h2 className="text-center mb-4">Add Product</h2>
+        <form onSubmit={productDetails.handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="productName" className="form-label">Product Name</label>
+            <input
+              type="text"
+              className="form-control"
+              id="name"
+              onChange={productDetails.handleChange}
+              value={productDetails.values.name}
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="productPrice" className="form-label">Product Price</label>
+            <input
+              type="number"
+              className="form-control"
+              id="price"
+              onChange={productDetails.handleChange}
+              value={productDetails.values.price}
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="productCategory" className="form-label">Product Category</label>
+            <input
+              type="text"
+              className="form-control"
+              id="category"
+              onChange={productDetails.handleChange}
+              value={productDetails.values.category}
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="productImage" className="form-label">Product Image</label>
+            <input
+              type="text"
+              className="form-control"
+              id="image"
+              onChange={productDetails.handleChange}
+              value={productDetails.values.image}
+              required
+            />
+          </div>
+          <button type="submit" className="btn btn-primary w-100">Submit</button>
+        </form>
+      </div>
     </div>
   )
 }
