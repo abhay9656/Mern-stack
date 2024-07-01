7c51c98a147248e3dc042f7a9d21966e896d5e5f
abhay9656 <abhay9656gupta@gmail.com>
2024-07-01 14:42:31 +0530

    Add user update form to update-user page component

--- Section 2/frontend/src/app/update-user/[id]/page.jsx
+++ Section 2/frontend/src/app/update-user/[id]/page.jsx
@@ -3,7 +3,47 @@ import React from 'react'
 const updateUser = () => {
   return (
     <div>
-      
+      <div className="container mt-5">
+      <h2>Update User Information</h2>
+      <form >
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            className="form-control"
+            id="name"
+            name="name"
+            
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            name="email"
+           
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            name="password"
+          
+            required
+          />
+        </div>
+        <button type="submit" className="btn btn-primary mt-4">
+          Update
+        </button>
+      </form>
+    </div>
     </div>
   )
 }
